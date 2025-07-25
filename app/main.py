5778c00268fe75575ae086145906e0abf99d05aa
Gokul Nath RK <gokulcyber9012@gmail.com>
2025-07-25 13:32:17 +0530

    modified main app

--- app/main.py
+++ app/main.py
@@ -1,18 +1,18 @@
-from flask import Flask, jsonify # type: ignore
-from pymongo import MongoClient # type: ignore
+from flask import Flask, jsonify #type:ignore
+from pymongo import MongoClient #type:ignore
+import os
 
 app = Flask(__name__)
-client = MongoClient("mongodb://admin:password@db-for-app:27017/")
-db = client["mydatabase"]
+
+# Get connection details from environment
+client = MongoClient(
+    f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASS')}"
+    f"@{os.getenv('MONGO_HOST')}:27017/"
+)
 
 @app.route("/")
 def home():
     return jsonify({"status": "Hello from Docker!", "db": "MongoDB connected"})
 
-@app.route("/users")
-def users():
-    users = list(db.users.find({}, {"_id": 0}))
-    return jsonify(users)
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
